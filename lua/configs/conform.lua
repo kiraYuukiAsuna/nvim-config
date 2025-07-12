d80a3035c28410a7000d2337eea309ed72e51d4f
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2025-07-13 00:23:08 +0800

    Update Config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,38 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+        -- Lua
+        lua = { "stylua" },
+        -- Web 开发
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        -- Shell
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        -- XML
+        xml = { "xmlformat" },
+        -- C/C++
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        -- Python
+        python = { "black", "isort" },
+        -- C#
+        cs = { "csharpier" },
+        csharp = { "csharpier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
 return options
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = 'both' -- to enable cursorline!
+
+-- TAB 和缩进设置
+o.tabstop = 4        -- TAB 显示为 4 个空格
+o.softtabstop = 4    -- 编辑时 TAB 的宽度
+o.shiftwidth = 4     -- 自动缩进时使用 4 个空格
+o.expandtab = true   -- 将 TAB 转换为空格
+o.smartindent = true -- 智能缩进
+o.autoindent = true  -- 自动缩进
+o.shiftround = true  -- 缩进时取整到 shiftwidth 的倍数
+
+o.shell = "pwsh.exe" -- 使用 PowerShell Core 作为默认 shell
